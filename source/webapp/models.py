5505fd550fa4a01e64a5e21e4bfb8dad70b2ec20
Yuburov <yubur.aziz@gmail.com>
2019-09-20 12:54:30 +0600

    Создал класс Product в manage.py, применил миграции

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+OTHER_CHOICE = 'other'
+CATEGORY_CHOICES = (
+    (OTHER_CHOICE, 'Другое'),
+    ('food', 'Еда'),
+    ('footwear', 'Обувь'),
+    ('T-shirts', 'Футболки'),
+    ('electronics', 'Электроника')
+)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название')
+    description = models.TextField(max_length=2000, verbose_name='Описание', null=True, blank=True)
+    category = models.CharField(max_length=20, verbose_name='Категория',
+                                choices=CATEGORY_CHOICES, default=OTHER_CHOICE)
+    amount = models.IntegerField(verbose_name='Остаток')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Цена')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
