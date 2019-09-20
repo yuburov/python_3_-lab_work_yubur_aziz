1fb2b1cd38df2fdddc9dfba36c8a8ad6f3f7c4b5
Yuburov <yubur.aziz@gmail.com>
2019-09-20 15:00:40 +0600

    Вывел список товаров на главную страницу

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import index_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+#     path('products/<int:pk>/', product_detail_view, name='product_detail')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from webapp.models import Product
 
-# Create your views here.
+
+def index_view(request):
+    products = Product.objects.filter(amount__gt=1).order_by('category','name')
+    return render(request, 'index.html', context={
+        'products': products
+    })
+
+#
+# def product_detail_view(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#
+#     return render(request, 'product.html', context={
+#         'product': product
+#     })
